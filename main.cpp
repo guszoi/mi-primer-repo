114056ba65038a82362bbf8a58b2d7c6695f81b9
GusZ <gustavozoireff@gmail.com>
2025-01-22 11:45:43 -0300

    3rd Commit GZ

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ int main()
 {
     cout << "Hello world!" << endl;
     cout << "\n" << "AFTER FIRST COMMIT" << endl;
+    cout << "\n" << "AFTER SECOND COMMIT Y UPTOGITHUB PUSH" << endl;
     return 0;
 }
